36e9024f487a7bf7ed4452c14ebd451aa2fd4091
lanath <valentin@viviersoft.com>
2024-12-18 17:38:32 +0100

    fix: removing dead code

--- internal/docker-compose/docker.go
+++ internal/docker-compose/docker.go
@@ -1,7 +1,6 @@
 package compose
 
 import (
-	"os"
 	"strings"
 	"unicode"
 
@@ -32,44 +31,4 @@ func RetrieveValideInterfacesFromDockerContainer() ([]string, error) {
 	logging.Sugar.Debugw("detected interfaces.", "interfaces", interfaces)
 
 	return interfaces, nil
-}
-
-func GenerateSSLWithDocker(sslPath string) error {
-	logging.Sugar.Debugw("Generating ssl cert.", "path", sslPath)
-
-	err := os.MkdirAll(sslPath, os.ModePerm)
-	if err != nil {
-		logging.Sugar.Errorw("cannot create cert containing folder.", "error", err)
-	}
-
-	alreadyHasNginx, err := docker.PullImageIfNotExisted("docker.io/library/", "nginx:1.27")
-	if err != nil {
-		logging.Sugar.Warnw("nginx pull failed", "error", err)
-		return err
-	}
-
-	_, err = docker.RunContainer("nginx", []string{
-		"openssl",
-		"req", "-new", "-nodes", "-x509",
-		"-subj", "/C=FR/ST=IDF/L=Paris/O=Stamus/CN=ClearNDR",
-		"-days", "3650",
-		"-keyout", "/etc/nginx/ssl/scirius.key",
-		"-out", "/etc/nginx/ssl/scirius.crt",
-		"-extensions", "v3_ca",
-	}, []string{
-		sslPath + ":/etc/nginx/ssl",
-	}, "")
-
-	if err != nil {
-		logging.Sugar.Infow("cannot generate cert.", "error", err)
-		return err
-	}
-
-	if !alreadyHasNginx {
-		logging.Sugar.Debug("nginx image was not previously installed, deleting.")
-		docker.DeleteDockerImageByName("", "nginx")
-	}
-
-	logging.Sugar.Debug("cert created.", "path", sslPath)
-	return nil
 }
