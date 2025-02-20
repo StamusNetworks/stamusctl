d21da3b3835c996e66e4e8ff141d9683c5351cb7
Baptiste Ternoir <bternoir@stamus-networks.com>
2025-02-20 15:21:17 +0100

    fix(handlers): clear instance remove networks

--- internal/handlers/config/clear.go
+++ internal/handlers/config/clear.go
@@ -2,16 +2,12 @@ package config
 
 import (
 	// Core
-	"bytes"
+
 	"fmt"
-	"log"
 	"os/exec"
-	"slices"
-	"strings"
 
 	// Internal
 	"stamus-ctl/internal/app"
-	docker "stamus-ctl/internal/docker-compose"
 	"stamus-ctl/internal/handlers/wrapper"
 	"stamus-ctl/internal/stamus"
 )
@@ -21,76 +17,19 @@ func Clear(conf string) error {
 	if !app.IsCtl() {
 		conf = app.GetConfigsFolder(conf)
 	}
-	// Get networks
-	log.Println("Getting networks")
-	networks, err := getNetworks(conf)
-	log.Println("Got networks", networks)
-	if err != nil {
-		return err
-	}
 	// Down containers
-	log.Println("Handling down")
-	err = wrapper.HandleDown(conf, true, true)
-	log.Println("Handled down with err", err)
-	if err != nil {
-		return err
-	}
-	// Clear networks
-	log.Println("Clearing networks", networks)
-	err = clearNetworks(networks)
-	log.Println("Cleared networks with err", err)
+	err := wrapper.HandleDown(conf, true, true)
 	if err != nil {
 		return err
 	}
 	// Delete folder
-	log.Println("Deleting folder", conf)
 	err = deleteFolder(conf)
-	log.Println("Deleted folder with err", err)
 	if err != nil {
 		return err
 	}
 	// Save stamus
-	log.Println("Removing instance", conf)
 	err = stamus.RemoveInstance(conf)
-	log.Println("Removed instance with err", err)
 	return err
-}
-
-func getNetworks(conf string) ([]string, error) {
-	var outBuf, errBuf bytes.Buffer
-	dockerCmpsFile := docker.GetComposeFilePath(conf)
-	externalCmd := exec.Command("docker", "compose", "-f", dockerCmpsFile, "ps", "--format", "{{.Networks}}")
-	externalCmd.Stdout = &outBuf
-	externalCmd.Stderr = &errBuf
-
-	err := externalCmd.Run()
-	if err != nil {
-		fmt.Println("Error:", err)
-		fmt.Println("Stderr:", errBuf.String())
-		return nil, err
-	}
-	// Make a list
-	networks := strings.Split(outBuf.String(), "\n")
-	// Remove empty strings, duplicates
-	toReturn := []string{}
-	for _, network := range networks {
-		if network != "" && !slices.Contains(toReturn, network) {
-			toReturn = append(toReturn, network)
-		}
-	}
-	return toReturn, nil
-}
-
-func clearNetworks(networks []string) error {
-	for _, network := range networks {
-		externalCmd := exec.Command("docker", "network", "rm", network)
-		err := externalCmd.Run()
-		if err != nil {
-			fmt.Println("Error:", err)
-			return err
-		}
-	}
-	return nil
 }
 
 func deleteFolder(conf string) error {
