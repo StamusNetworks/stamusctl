d3082561663320d5a0b414f4f2163401eb9eb446
Baptiste Ternoir <bternoir@stamus-networks.com>
2025-04-18 10:04:15 +0200

    fix(app): correct dev ctl version
    When running in dev mode, the version loaded by the ctl was not correct, now it is based on the `VERSION` file.

--- internal/app/version.go
+++ internal/app/version.go
@@ -1,7 +1,29 @@
 package app
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/afero"
+)
+
 var (
 	Arch    = ""
 	Commit  = ""
 	Version = "0.1"
 )
+
+func init() {
+	// Read version file in dev mode
+	execPath, _ := os.Executable()
+	execName := filepath.Base(execPath)
+	if execName == "cmd" {
+		content, err := afero.ReadFile(FS, "VERSION")
+		if err != nil {
+			fmt.Println("Error reading version file:", err)
+		} else {
+			Version = string(content)
+		}
+	}
+}
